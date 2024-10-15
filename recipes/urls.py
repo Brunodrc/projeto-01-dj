5748ade5f719bc19a9cbe54e52814118af1a4f37
Brunodrc <bruno.bdrc@gmail.com>
2024-10-14 21:32:21 -0300

    injeção de dados fakes para testes

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipes, name='recipes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+from .utils.recipes.factory_recipe import make_recipe
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    context = {
+        'recipes': [make_recipe() for _ in range(10)],
+    }
+    return render(request, 'recipes/pages/home.html', context)
+
+
+def recipes(request, id):
+    context = {
+        'recipe': make_recipe()
+    }
+    return render(request, 'recipes/pages/recipe-view.html', context)
