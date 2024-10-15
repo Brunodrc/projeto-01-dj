4fc9ad7387f8457a25e7872e8137b65f434102fa
Brunodrc <bruno.bdrc@gmail.com>
2024-10-15 14:47:25 -0300

    modelos criados e registrados no django admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+
+# superuser:adm_projeto01
+# a9prj33
