21d7f48fdad5c54c471b78fcfb1aa40c99256dcc
Brunodrc <bruno.bdrc@gmail.com>
2025-02-01 11:31:12 -0300

    correção de testes que estavam quebrando

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-# from django.http import Http404
+from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 # from .utils.recipes.factory_recipe import make_recipe
 from .models import Recipe
@@ -48,4 +48,8 @@ def recipes(request, id):
 
 
 def search(request):
+    search_term = request.GET.get('search')
+
+    if not search_term:
+        raise Http404()
     return render(request, 'recipes/pages/search.html')
