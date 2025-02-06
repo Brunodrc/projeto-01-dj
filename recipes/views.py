93533c892f61b58f3705620b4a7a93cd7e0be76e
Brunodrc <bruno.bdrc@gmail.com>
2025-02-06 17:59:36 -0300

    view search concluida

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-# from .utils.recipes.factory_recipe import make_recipe
 from .models import Recipe
+from django.db.models import Q
 
+# from .utils.recipes.factory_recipe import make_recipe
 # functions based views
 
 
@@ -52,7 +53,14 @@ def search(request):
 
     if not search_term:
         raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
