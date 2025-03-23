ffc1da3c441ecb9f6c41a61646a18fe71dc1aea4
Bruno D R Castro <bruno.bdrc@gmail.com>
2025-03-23 11:06:25 -0300

    construindo a logica de paginação intervalo do inicio ao meio da quantidade todal de paginas

--- recipes/utils/test_pagination.py
+++ recipes/utils/test_pagination.py
@@ -6,10 +6,33 @@ class PaginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
-            qty_paginas=4,
+            qty_pages=4,
             current_page=1,
         )
         self.assertEqual([1, 2, 3, 4], pagination)
 
-    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        ...
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self): # noqa E501
+        # pagina atual = 1, quantidade de paginas 2, pagina do meio 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # pagina atual = 2, quantidade de paginas 2, pagina do meio 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # pagina atual = 3, quantidade de paginas 2, pagina do meio 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
