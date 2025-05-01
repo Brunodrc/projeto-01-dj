5f40e0d282efeddd1467993d0da4a32af6a4e6a4
Brunodrc <bruno.bdrc@gmail.com>
2025-05-01 08:55:28 -0300

    funções auxiliares para sobrescrever campos do MoldelForm

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
 
+# função para facilitar as sobrescritas dos padrões do django
+
+
+def add_attrs(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+# função auxiliar especifica para placeholder
+
+
+def add_placeholder(field, placeholder_val):
+    add_attrs(field, 'placeholder', placeholder_val)
+
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput()
+    )
+
     class Meta:
         model = User
         fields = [
